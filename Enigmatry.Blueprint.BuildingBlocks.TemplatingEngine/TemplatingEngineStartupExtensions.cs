594ff5e84fcfe8ef75e01ebb031c8a552c0724f8
Edwin de Koning <eko@enigmatry.com>
2020-09-03 17:14:42 +0200

    Added TemplatingEngineStartupExtensions

---
+++ Enigmatry.Blueprint.BuildingBlocks.TemplatingEngine/TemplatingEngineStartupExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Enigmatry.Blueprint.BuildingBlocks.TemplatingEngine
+{
+    public static class TemplatingEngineStartupExtensions
+    {
+        public static void AppAddTemplatingEngine(this IServiceCollection services)
+        {
+            services.AddScoped<ITemplatingEngine, RazorTemplatingEngine>();
+        }
+    }
+}
