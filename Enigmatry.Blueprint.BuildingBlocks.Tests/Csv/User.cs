152d4ebe7fdbc0f6feb5a1a17de9cbc68fb4002b
Edwin de Koning <eko@enigmatry.com>
2020-10-05 17:31:12 +0200

    A very basic Csv Helper, with Tests

---
+++ Enigmatry.Blueprint.BuildingBlocks.Tests/Csv/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enigmatry.Blueprint.BuildingBlocks.Tests.Csv
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
