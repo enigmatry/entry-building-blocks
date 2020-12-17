919b5d0a8697ee1d9e06d8dea19fa95680bab732
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2020-12-17 17:33:46 +0100

    Added building block for AspNetCoreTests

---
+++ Enigmatry.Blueprint.BuildingBlocks.AspNetCore.Tests/Http/AssertionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace Enigmatry.Blueprint.BuildingBlocks.AspNetCore.Tests.Http
+{
+    public static class AssertionExtensions
+    {
+        public static HttpResponseAssertions Should(this HttpResponseMessage actualValue) => new HttpResponseAssertions(actualValue);
+    }
+}
