3a3421c12d11a5e2882610e665d9109d0d85aef8
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2020-12-22 09:15:43 +0100

    Read DropAllSql through a helper class

---
+++ Enigmatry.Blueprint.BuildingBlocks.AspNetCore.Tests/Database/DatabaseHelpers.cs
@@ -0,0 +1,11 @@
+﻿// unset
+
+namespace Enigmatry.Blueprint.BuildingBlocks.AspNetCore.Tests.Database
+{
+    public static class DatabaseHelpers
+    {
+        public static string DropAllSql =>
+            EmbeddedResource.ReadResourceContent(
+                "Enigmatry.Blueprint.BuildingBlocks.AspNetCore.Tests.Database.DropAllSql.sql");
+    }
+}
