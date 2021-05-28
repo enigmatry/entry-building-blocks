de073cadcf35526b02915c19e5d7f4ad05f6dd51
Edwin de Koning <eko@enigmatry.com>
2021-05-28 11:29:34 +0200

    More nullability issues

--- Enigmatry.BuildingBlocks.AspNetCore.Tests/Http/JsonHttpClientExtensions.cs
+++ Enigmatry.BuildingBlocks.AspNetCore.Tests/Http/JsonHttpClientExtensions.cs
@@ -7,13 +7,13 @@ namespace Enigmatry.BuildingBlocks.AspNetCore.Tests.Http
 {
     public static class JsonHttpClientExtensions
     {
-        public static async Task<T> GetAsync<T>(this HttpClient client, string uri)
+        public static async Task<T?> GetAsync<T>(this HttpClient client, string uri)
         {
             HttpResponseMessage response = await client.GetAsync(uri);
             return await response.DeserializeWithStatusCodeCheckAsync<T>();
         }
 
-        public static async Task<T> GetAsync<T>(this HttpClient client, Uri uri,
+        public static async Task<T?> GetAsync<T>(this HttpClient client, Uri uri,
             params KeyValuePair<string, string>[] parameters)
         {
             var resourceUri = uri.AppendParameters(parameters);
@@ -28,7 +28,7 @@ namespace Enigmatry.BuildingBlocks.AspNetCore.Tests.Http
             await response.EnsureSuccessStatusCodeAsync();
         }
 
-        public static async Task<TResponse> PutAsync<T, TResponse>(this HttpClient client, string uri, T content)
+        public static async Task<TResponse?> PutAsync<T, TResponse>(this HttpClient client, string uri, T content)
         {
             HttpResponseMessage response = await client.PutAsJsonAsync(uri, content);
             return await response.DeserializeWithStatusCodeCheckAsync<TResponse>();
@@ -40,7 +40,7 @@ namespace Enigmatry.BuildingBlocks.AspNetCore.Tests.Http
             await response.EnsureSuccessStatusCodeAsync();
         }
 
-        public static async Task<TResponse> PostAsync<T, TResponse>(this HttpClient client, string uri, T content)
+        public static async Task<TResponse?> PostAsync<T, TResponse>(this HttpClient client, string uri, T content)
         {
             HttpResponseMessage response = await client.PostAsJsonAsync(uri, content);
             return await response.DeserializeWithStatusCodeCheckAsync<TResponse>();
