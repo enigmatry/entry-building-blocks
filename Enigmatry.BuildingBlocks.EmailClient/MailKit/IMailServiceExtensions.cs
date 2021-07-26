93097d426136791d1377f217e1a9972674c71480
Edwin de Koning <eko@enigmatry.com>
2021-07-26 10:52:32 +0200

    Fixed issue when sending email over non-standard ssl ports (like with Office365)

--- Enigmatry.BuildingBlocks.EmailClient/MailKit/IMailServiceExtensions.cs
+++ Enigmatry.BuildingBlocks.EmailClient/MailKit/IMailServiceExtensions.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Enigmatry.BuildingBlocks.Core.Settings;
 using MailKit;
+using MailKit.Security;
 
 namespace Enigmatry.BuildingBlocks.Email.MailKit
 {
@@ -10,7 +11,16 @@ namespace Enigmatry.BuildingBlocks.Email.MailKit
     {
         internal static async Task ConnectAsync(this IMailService mailService, SmtpSettings settings, CancellationToken cancellationToken = default)
         {
-            await mailService.ConnectAsync(settings.Server, settings.Port, true, cancellationToken);
+            if (settings.Port == 465)
+            {
+                await mailService.ConnectAsync(settings.Server, settings.Port, true, cancellationToken);
+            }
+            else
+            {
+                // To support smtp over non-standard SSL ports (like Office365, which uses port 587)
+                await mailService.ConnectAsync(settings.Server, settings.Port, SecureSocketOptions.StartTls, cancellationToken);
+            }
+
             if (!String.IsNullOrEmpty(settings.Username) && !String.IsNullOrEmpty(settings.Password))
             {
                 await mailService.AuthenticateAsync(settings.Username, settings.Password, cancellationToken);
