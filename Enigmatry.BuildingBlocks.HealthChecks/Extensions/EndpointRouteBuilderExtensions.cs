f97849b8c5ca42b6535909c751be3df1d1aa3813
Nikola Dragicevic <nda@enigmatry.com>
2022-08-18 16:22:27 +0000

    Merged PR 4041: Building Block: Improvements for HealthCheck
    https://jira.enigmatry.com/browse/ETL-340

---
+++ Enigmatry.BuildingBlocks.HealthChecks/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Enigmatry.BuildingBlocks.HealthChecks.Authorization;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Enigmatry.BuildingBlocks.HealthChecks.Extensions
+{
+    [PublicAPI]
+    public static class StartupExtensions
+    {
+        public static void AppMapHealthCheck(this IEndpointRouteBuilder endpoints, IConfiguration configuration)
+        {
+            var settings = configuration.ResolveHealthCheckSettings();
+            var healthCheckEndpoint = endpoints.MapHealthChecks("/healthcheck", new HealthCheckOptions
+            {
+                // Specify a custom ResponseWriter, so we can return json with additional information,
+                // Otherwise it will just return plain text with the status.
+                ResponseWriter = WriteResponse,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                }
+            });
+
+            if (settings.TokenAuthorizationEnabled)
+            {
+                healthCheckEndpoint.RequireAuthorization(TokenRequirement.Name);
+            }
+        }
+
+        private static async Task WriteResponse(HttpContext context, HealthReport report) =>
+            await context.Response.WriteAsJsonAsync(
+                new
+                {
+                    status = report.Status.ToString(),
+                    entries = report.Entries.Select(keyValuePair =>
+                        new { key = keyValuePair.Key, value = keyValuePair.Value.Status.ToString() })
+                }, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
---
+++ Enigmatry.BuildingBlocks.HealthChecks/Settings.cs
@@ -0,0 +1,16 @@
+﻿using Enigmatry.BuildingBlocks.Core.Helpers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Enigmatry.BuildingBlocks.HealthChecks
+{
+    [ExcludeFromCodeCoverage]
+    internal class Settings
+    {
+        internal const string SectionName = "HealthChecks";
+
+        internal int MaximumAllowedMemoryInMegaBytes { get; set; }
+        internal string RequiredToken { get; set; } = string.Empty;
+
+        internal bool TokenAuthorizationEnabled => RequiredToken.HasContent();
+    }
+}
