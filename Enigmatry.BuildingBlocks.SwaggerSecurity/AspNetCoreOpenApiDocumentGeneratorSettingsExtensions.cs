fe30cee85ead3d988033d0b56ff46b61c6c31b30
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2022-08-19 14:13:05 +0200

    BP-282: Add support for automatic marking of non-nullable properties as required.

---
+++ Enigmatry.BuildingBlocks.SwaggerSecurity/AspNetCoreOpenApiDocumentGeneratorSettingsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NSwag.Generation.AspNetCore;
+
+namespace Enigmatry.BuildingBlocks.Swagger
+{
+    internal static class AspNetCoreOpenApiDocumentGeneratorSettingsExtensions
+    {
+        internal static void ConfigureSwaggerSettings(this AspNetCoreOpenApiDocumentGeneratorSettings settings,
+            string appTitle, string appVersion, Action<AspNetCoreOpenApiDocumentGeneratorSettings>? configureSettings)
+        {
+            settings.DocumentName = appVersion;
+            settings.Title = appTitle;
+            settings.Version = appVersion;
+            settings.SchemaNameGenerator = new CustomSwaggerSchemaNameGenerator();
+
+            configureSettings?.Invoke(settings);
+        }
+
+        public static AspNetCoreOpenApiDocumentGeneratorSettings MarkNonNullablePropertiesAsRequired(
+            this AspNetCoreOpenApiDocumentGeneratorSettings settings, bool enable)
+        {
+            if (enable)
+            {
+                settings.SchemaProcessors.Add(new MarkAsRequiredIfNonNullableSchemaProcessor());
+            }
+
+            return settings;
+        }
+    }
+}
