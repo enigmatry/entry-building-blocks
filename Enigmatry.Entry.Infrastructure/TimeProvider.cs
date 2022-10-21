d1e53dc62663377014b5f99df47761f4d2918112
Nikola Dragićević <nda@enigmatry.com>
2022-10-21 10:44:39 +0200

    https://jira.enigmatry.com/browse/ETL-337 ETL-337 Building Block: Improvements Rework after code review.

--- Enigmatry.Entry.Infrastructure/TimeProvider.cs
+++ Enigmatry.Entry.Infrastructure/TimeProvider.cs
@@ -4,22 +4,12 @@ using System;
 
 namespace Enigmatry.Entry.Infrastructure
 {
-    /// <summary>
-    /// Class represents implemented abstraction over time management. Makes any time-based code easily testable.
-    /// </summary>
     [PublicAPI]
     public class TimeProvider : ITimeProvider
     {
         private readonly Lazy<DateTimeOffset> _now = new(() => DateTimeOffset.UtcNow);
 
-        /// <summary>
-        /// Returns current UTC time for the first invocation. Subsequent invocations return cached value (the same value as in first invocation).
-        /// </summary>
         public DateTimeOffset FixedUtcNow => _now.Value;
-
-        /// <summary>
-        /// Always returns current UTC time, no matter how many times you invoke it.
-        /// </summary>
         public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
     }
 }
