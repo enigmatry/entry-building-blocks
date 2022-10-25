3f4b3580265d8ae5650ca5c90ef2d41d2a6b459f
Nikola Dragicevic <nda@enigmatry.com>
2022-10-25 09:45:00 +0000

    Merged PR 5332: Building Blocks: Add Https redirection & HSTS setting extension
    https://jira.enigmatry.com/browse/ETL-419

---
+++ Enigmatry.Entry.AspNetCore/Security/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net;
+
+namespace Enigmatry.Entry.AspNetCore.Security
+{
+    [PublicAPI]
+    public static class ServiceCollectionExtensions
+    {
+        public static void AppAddHttps(this IServiceCollection services, IHostEnvironment environment)
+        {
+            var isDevelopmentEnvironment = environment.IsDevelopment();
+
+            if (!isDevelopmentEnvironment)
+            {
+                const int oneYear = 365;
+                services.AddHsts(options =>
+                {
+                    options.Preload = true;
+                    options.IncludeSubDomains = true;
+                    options.MaxAge = TimeSpan.FromDays(oneYear);
+                });
+            }
+
+            var redirection = isDevelopmentEnvironment
+                ? HttpStatusCode.TemporaryRedirect
+                : HttpStatusCode.PermanentRedirect;
+
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = (int)redirection;
+            });
+        }
+    }
+}
