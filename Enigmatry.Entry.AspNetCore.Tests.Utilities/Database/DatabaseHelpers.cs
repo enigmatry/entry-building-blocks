f3c8b5b59dd00055d1bd60f11af9cff37a15117f
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2023-03-31 13:14:22 +0200

    Fix namespace name in DatabaseHelpers

--- Enigmatry.Entry.AspNetCore.Tests.Utilities/Database/DatabaseHelpers.cs
+++ Enigmatry.Entry.AspNetCore.Tests.Utilities/Database/DatabaseHelpers.cs
@@ -1,10 +1,12 @@
 ﻿using Enigmatry.Entry.Core.Helpers;
+using JetBrains.Annotations;
 
 namespace Enigmatry.Entry.AspNetCore.Tests.Utilities.Database;
 
+[UsedImplicitly]
 public static class DatabaseHelpers
 {
     public static string DropAllSql =>
         EmbeddedResource.ReadResourceContent(
-            "Enigmatry.Entry.AspNetCore.Tests.Database.DropAllSql.sql", typeof(DatabaseHelpers).Assembly);
+            $"{typeof(DatabaseHelpers).Namespace}.DropAllSql.sql", typeof(DatabaseHelpers).Assembly);
 }
