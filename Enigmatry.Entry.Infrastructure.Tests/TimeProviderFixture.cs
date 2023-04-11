5312f34d2f6b1191a25b736995bf29b8794ea8be
Vladan Šljivić <vladan.sljivic@novalite.rs>
2023-04-11 14:26:36 +0200

    Fixing flaky test (TimeProviderFixture.UniqueTime)

--- Enigmatry.Entry.Infrastructure.Tests/TimeProviderFixture.cs
+++ Enigmatry.Entry.Infrastructure.Tests/TimeProviderFixture.cs
@@ -7,22 +7,24 @@ namespace Enigmatry.Entry.Infrastructure.Tests;
 public class TimeProviderFixture
 {
     [Test]
-    public void SameTime()
+    public async Task SameTime()
     {
         var provider = new TimeProvider();
 
         var firstDate = provider.FixedUtcNow;
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         var secondDate = provider.FixedUtcNow;
 
         firstDate.Should().BeExactly(secondDate);
     }
 
     [Test]
-    public void UniqueTime()
+    public async Task UniqueTime()
     {
         var provider = new TimeProvider();
 
         var firstDate = provider.UtcNow;
+        await Task.Delay(TimeSpan.FromMilliseconds(1));
         var secondDate = provider.UtcNow;
 
         firstDate.Should().BeBefore(secondDate);
