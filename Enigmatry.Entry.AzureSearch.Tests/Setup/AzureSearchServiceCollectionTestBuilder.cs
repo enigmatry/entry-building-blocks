e71b71545de88b265b795548492ee884b8624086
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2023-04-18 16:17:01 +0200

    BP-477: Add reading of AzureSearch ApiKey and ServiceEndpoint from either user secrets or environment variable

--- Enigmatry.Entry.AzureSearch.Tests/Setup/AzureSearchServiceCollectionTestBuilder.cs
+++ Enigmatry.Entry.AzureSearch.Tests/Setup/AzureSearchServiceCollectionTestBuilder.cs
@@ -1,21 +1,51 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Enigmatry.Entry.AzureSearch.Tests.Setup;
 
 public class AzureSearchServiceCollectionTestBuilder
 {
-    private readonly ServiceCollection _serviceCollection = new();
+    private const string AzureSearchApiKey = "AzureSearch:ApiKey";
+    private const string AzureSearchSearchServiceEndpointKey = "AzureSearch:SearchServiceEndPoint";
+
+    private readonly ServiceCollection _services = new();
     public ServiceProvider Build()
     {
-        _serviceCollection.AddAzureSearch(configure =>
+        _services.AddLogging(configure => configure.AddConsole());
+
+        var configuration = CreateConfiguration();
+        _services.AddScoped<IConfiguration>(_ => configuration);
+
+        _services.AddAzureSearch(configure =>
             {
-                configure.ApiKey = "";
-                configure.SearchServiceEndPoint = new Uri("https://enigmatry-dev.search.windows.net");
+                configure.ApiKey = configuration[AzureSearchApiKey];
+                configure.SearchServiceEndPoint = new Uri(configuration[AzureSearchSearchServiceEndpointKey]);
             })
             .AddDocument<TestDocument>()
             .AddSearchIndexFactory<TestDocumentSearchIndexFactory>()
             .AddDocument<AnotherTestDocument>();
 
-        return _serviceCollection.BuildServiceProvider();
+        return _services.BuildServiceProvider();
+    }
+
+    private static IConfigurationRoot CreateConfiguration()
+    {
+        var configurationSource = new MemoryConfigurationSource
+        {
+            InitialData = new Dictionary<string, string>
+            {
+                { AzureSearchApiKey,  "USER_SECRET"},
+                { AzureSearchSearchServiceEndpointKey, "USER_SECRET" }
+            }
+        };
+
+        var configurationBuilder = new ConfigurationBuilder()
+            .Add(configurationSource)
+            .AddUserSecrets<AzureSearchServiceCollectionTestBuilder>()
+            .AddEnvironmentVariables();
+
+        return configurationBuilder.Build();
     }
 }
