8a2b9b5689bdbc0fa5d59a8345e90a7d94705916
Vladan Šljivić <vladan.sljivic@novalite.rs>
2023-07-28 13:18:19 +0200

    Improving UserHasPermissionPolicyProvider: - injecting AuthorizationOptions so default or fallback policies can be defined from the outside - falling back to default implementation in case when UserHasPermissionAttribute is not used

--- Enigmatry.Entry.AspNetCore.Authorization/UserHasPermissionPolicyProvider.cs
+++ Enigmatry.Entry.AspNetCore.Authorization/UserHasPermissionPolicyProvider.cs
@@ -1,25 +1,34 @@
 ﻿using Enigmatry.Entry.AspNetCore.Authorization.Attributes;
 using Enigmatry.Entry.AspNetCore.Authorization.Requirements;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
 
 namespace Enigmatry.Entry.AspNetCore.Authorization;
 
-internal class UserHasPermissionPolicyProvider<T> : IAuthorizationPolicyProvider where T : notnull
+internal class UserHasPermissionPolicyProvider<TPermission> : IAuthorizationPolicyProvider where TPermission : notnull
 {
+    private readonly DefaultAuthorizationPolicyProvider _defaultPolicyProvider;
+
+    public UserHasPermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+    {
+        _defaultPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+    }
+
     public Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
     {
-        if (policyName.StartsWith(UserHasPermissionAttribute<T>.PolicyPrefix, StringComparison.OrdinalIgnoreCase))
+        if (policyName.StartsWith(UserHasPermissionAttribute<TPermission>.PolicyPrefix, StringComparison.OrdinalIgnoreCase))
         {
+            // Caching could be added here; AuthorizationPolicy can be created once per policyName and cached.
             var requirement =
-                new UserHasPermissionRequirement<T>(PermissionTypeConverter<T>.ConvertFromPolicyName(UserHasPermissionAttribute<T>.PolicyPrefix, policyName));
+                new UserHasPermissionRequirement<TPermission>(
+                    PermissionTypeConverter<TPermission>.ConvertFromPolicyName(UserHasPermissionAttribute<TPermission>.PolicyPrefix, policyName));
             return Task.FromResult(new AuthorizationPolicyBuilder().AddRequirements(requirement).Build())!;
         }
 
-        return GetDefaultPolicyAsync()!;
+        return _defaultPolicyProvider.GetPolicyAsync(policyName);
     }
 
-    // TODO:This also adds the policy to swagger and healthcheck endpoints. Need to find a solution
-    public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => Task.FromResult(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build());
+    public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => _defaultPolicyProvider.GetDefaultPolicyAsync(); // DefaultPolicy is RequireAuthenticatedUser
 
-    public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => GetDefaultPolicyAsync()!;
+    public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => _defaultPolicyProvider.GetFallbackPolicyAsync();
 }
