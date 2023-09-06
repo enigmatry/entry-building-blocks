841a89537ed53fcdf2138af133181ccaebb33717
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2023-09-06 15:00:41 +0200

    BP-666: Change AppUseExceptionHandler to accept async callback function for onException

--- Enigmatry.Entry.AspNetCore/Exceptions/ApplicationBuilderExtensions.cs
+++ Enigmatry.Entry.AspNetCore/Exceptions/ApplicationBuilderExtensions.cs
@@ -2,18 +2,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System;
+using System.Threading.Tasks;
 
 namespace Enigmatry.Entry.AspNetCore.Exceptions;
 
 [PublicAPI]
 public static class ApplicationBuilderExtensions
 {
-    public static void AppUseExceptionHandler(this IApplicationBuilder builder, Func<HttpContext, bool>? onBeforeException = null) =>
+    public static void AppUseExceptionHandler(this IApplicationBuilder builder,
+        Func<HttpContext, Task<bool>>? onBeforeException = null) =>
         builder.UseExceptionHandler(exceptionHandlerApp =>
         {
             exceptionHandlerApp.Run(async context =>
             {
-                var handledElsewhere = onBeforeException?.Invoke(context) ?? false;
+                var handledElsewhere = false;
+                if (onBeforeException != null)
+                {
+                    handledElsewhere = await onBeforeException.Invoke(context);
+                }
+
                 if (handledElsewhere)
                 {
                     return;
