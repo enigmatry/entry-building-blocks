455440b2374cb7f2247c4b7411b49538f808fdac
Edwin de Koning <eko@enigmatry.com>
2023-10-19 14:46:12 +0200

    Removed old tests project

--- Enigmatry.Entry.Tests/Core/Images/Source.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Enigmatry.Entry.Tests.Core.Images
-{
-    internal class Source
-    {
-        internal static StringBytes Valid => new() { DataUri = "e+o=", Bytes = new byte[] { 123, 234 } };
-    }
-}
