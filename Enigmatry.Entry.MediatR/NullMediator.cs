0d9c241f8cdd3c39c8eb29a8711f10078dbdc51b
Andries van Waas <a.vanwaas@enigmatry.com>
2023-10-24 23:57:34 +0200

    update MediatR to 12.1.1

--- Enigmatry.Entry.MediatR/NullMediator.cs
+++ Enigmatry.Entry.MediatR/NullMediator.cs
@@ -1,34 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 
-namespace Enigmatry.Entry.MediatR
+namespace Enigmatry.Entry.MediatR;
+
+public class NullMediator : IMediator
 {
-    public class NullMediator : IMediator
-    {
-        public Task Publish(object notification, CancellationToken cancellationToken = default) =>
-            Task.CompletedTask;
+    public Task Publish(object notification, CancellationToken cancellationToken = default) =>
+        Task.CompletedTask;
 
-        public Task Publish<TNotification>(TNotification notification,
-            CancellationToken cancellationToken = default) where TNotification : INotification =>
-            Task.CompletedTask;
+    public Task Publish<TNotification>(TNotification notification,
+        CancellationToken cancellationToken = default) where TNotification : INotification =>
+        Task.CompletedTask;
 
-        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request,
-            CancellationToken cancellationToken = default) =>
-            Task.FromResult(default(TResponse)!);
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request,
+        CancellationToken cancellationToken = default) =>
+        Task.FromResult(default(TResponse)!);
 
-        public Task<object?> Send(object request, CancellationToken cancellationToken = default) =>
-            Task.FromResult(default(object));
+    public Task Send<TRequest>(TRequest request, CancellationToken cancellationToken = new())
+        where TRequest : IRequest => Task.CompletedTask;
 
-#if NETSTANDARD2_1_OR_GREATER
-        public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request,
-            CancellationToken cancellationToken = new()) =>
-            throw new System.NotImplementedException();
+    public Task<object?> Send(object request, CancellationToken cancellationToken = default) =>
+        Task.FromResult(default(object));
 
-        public IAsyncEnumerable<object?> CreateStream(object request,
-            CancellationToken cancellationToken = new()) =>
-            throw new System.NotImplementedException();
-#endif
-    }
+    public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request,
+        CancellationToken cancellationToken = new()) =>
+        throw new NotImplementedException();
+
+    public IAsyncEnumerable<object?> CreateStream(object request,
+        CancellationToken cancellationToken = new()) =>
+        throw new NotImplementedException();
 }
