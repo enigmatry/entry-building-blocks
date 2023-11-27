c0f7731340777076c0c724035a3dddcf3d6a4f0d
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-11-27 12:01:15 +0100

    Remove UseAccessToken.

--- Enigmatry.Entry.Core/Settings/DbContextSettings.cs
+++ Enigmatry.Entry.Core/Settings/DbContextSettings.cs
@@ -4,8 +4,6 @@ namespace Enigmatry.Entry.Core.Settings
 {
     public class DbContextSettings
     {
-        public bool UseAccessToken { get; set; }
-
         public bool SensitiveDataLoggingEnabled { get; set; }
 
         public int ConnectionResiliencyMaxRetryCount { get; set; }
