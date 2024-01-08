d88b24a0bb19e990ef9d9fccebf9c0607b37ae39
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2024-01-08 13:36:36 +0100

    BP-789: Add Enigmatry.Entry.MediatR.Tests and corresponding test that confirms that ValidationBehaviorPipeline is not working for requests without response

---
+++ Enigmatry.Entry.MediatR.Tests/Samples/RequestWithResponse.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using JetBrains.Annotations;
+using MediatR;
+
+namespace Enigmatry.Entry.MediatR.Tests.Samples;
+
+public class RequestWithResponse : IRequest<Response>
+{
+    public string Name { get; set; } = string.Empty;
+
+    [UsedImplicitly]
+    public class Validator : AbstractValidator<RequestWithResponse>
+    {
+        public Validator()
+        {
+            // minimum validation rule just to see if validation works
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
---
+++ Enigmatry.Entry.MediatR.Tests/Samples/Response.cs
@@ -0,0 +1,6 @@
+﻿namespace Enigmatry.Entry.MediatR.Tests.Samples;
+
+public class Response
+{
+    public string Handled { get; set; } = string.Empty;
+}
