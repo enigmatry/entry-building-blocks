4c5fb8f92555fa2dbc4bc5f2fd1162b60c305dab
zsrdjan <srdjan.zivojinovic@crafters.cloud>
2024-02-19 12:17:43 +0100

    BP-940: Replace InstrumentationKey with ConnectionString (#66)

--- Enigmatry.Entry.Core/Settings/ApplicationInsightsSettings.cs
+++ Enigmatry.Entry.Core/Settings/ApplicationInsightsSettings.cs
@@ -2,14 +2,18 @@
 using JetBrains.Annotations;
 using Serilog.Events;
 
-namespace Enigmatry.Entry.Core.Settings
-{
-    [UsedImplicitly]
-    public class ApplicationInsightsSettings
-    {
-        public const string ApplicationInsightsSectionName = "ApplicationInsights";
+namespace Enigmatry.Entry.Core.Settings;
 
-        public string InstrumentationKey { get; set; } = string.Empty;
-        public LogEventLevel SerilogLogsRestrictedToMinimumLevel { get; set; }
-    }
+[PublicAPI]
+public class ApplicationInsightsSettings
+{
+    public const string ApplicationInsightsSectionName = "ApplicationInsights";
+
+    [Obsolete("Migrate to use ConnectionString instead.")]
+    public string InstrumentationKey { get; set; } = string.Empty;
+
+    public string ConnectionString { get; set; } = string.Empty;
+
+    [Obsolete("Switch to new way of integrating AppInsights with Serilog - https://github.com/serilog-contrib/serilog-sinks-applicationinsights.")]
+    public LogEventLevel SerilogLogsRestrictedToMinimumLevel { get; set; }
 }
