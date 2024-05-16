7c0046a05a01f6ae5028272d62bab0888879507e
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-05-16 16:54:52 +0200

    Fix csv test.

--- Enigmatry.Entry.Csv.Tests/CsvFixture.cs
+++ Enigmatry.Entry.Csv.Tests/CsvFixture.cs
@@ -18,7 +18,7 @@ public class CsvFixture
                 FirstName = "John",
                 LastName = "Doe",
                 Age = 30,
-                LastLogon = new DateTimeOffset(2023, 12, 19, 9, 30, 0, DateTimeOffset.Now.Offset),
+                LastLogon = new DateTimeOffset(2023, 12, 19, 9, 30, 0, TimeSpan.FromHours(1)),
                 SomeDateTime = new DateTime(2022, 4, 27, 9, 30, 0)
             }
         };
