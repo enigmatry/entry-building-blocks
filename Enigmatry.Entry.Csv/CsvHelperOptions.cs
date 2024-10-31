6ae0e96337d1bcb8f92b2bf1269e22b6a5a10cd9
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2024-10-31 14:53:19 +0100

    BP-1244: Add option to replace CSV column names (localize them) through the CsvHelperOptions.cs

---
+++ Enigmatry.Entry.Csv/CsvHelperOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Enigmatry.Entry.Csv;
+
+public class CsvHelperOptions
+{
+    public CultureInfo Culture { get; private set; } = CultureInfo.CurrentCulture;
+    public Func<string, string> ColumnNameReplacer { get; private set; } = name => name;
+
+    public CsvHelperOptions WithCulture(CultureInfo culture)
+    {
+        Culture = culture;
+        return this;
+    }
+
+    public CsvHelperOptions WithColumnNameReplacer(Func<string, string> nameReplacer)
+    {
+        ColumnNameReplacer = nameReplacer;
+        return this;
+    }
+}
