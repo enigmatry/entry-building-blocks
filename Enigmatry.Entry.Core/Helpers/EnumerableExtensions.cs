90a2950fb11b8c9fe98c32160ee1993efb478bfb
laslorajda <45570435+laslorajda@users.noreply.github.com>
2025-04-01 10:07:37 +0200

    Removed redundant DistinctBy (#91)
    Co-authored-by: Laslo Rajda <lra@novalite.rs>

--- Enigmatry.Entry.Core/Helpers/EnumerableExtensions.cs
+++ Enigmatry.Entry.Core/Helpers/EnumerableExtensions.cs
@@ -1,37 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace Enigmatry.Entry.Core.Helpers;
 
-namespace Enigmatry.Entry.Core.Helpers
+public static class EnumerableExtensions
 {
-    public static class EnumerableExtensions
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
     {
-        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        ArgumentNullException.ThrowIfNull(action);
+
+        foreach (var item in source)
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
-            foreach (var item in source)
-            {
-                action(item);
-            }
-        }
-
-        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? source) => source ?? Enumerable.Empty<T>();
-
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            var knownKeys = new HashSet<TKey>();
-            foreach (var element in source)
-            {
-                if (knownKeys.Add(keySelector(element)))
-                {
-                    yield return element;
-                }
-            }
+            action(item);
         }
     }
+
+    public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? source) => source ?? [];
 }
 
