d1da2adc2bff7eb802308bba88cbbea1dbfd6042
Laslo Rajda <lra@novalite.rs>
2025-05-13 12:27:47 +0200

    BP-1291: Set default page size to valid value

--- Enigmatry.Entry.Core/Paging/PagedRequest.cs
+++ Enigmatry.Entry.Core/Paging/PagedRequest.cs
@@ -5,7 +5,7 @@ namespace Enigmatry.Entry.Core.Paging;
 public class PagedRequest<TResponse> : IRequest<PagedResponse<TResponse>>, IPagedRequest
 {
     public int PageNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
+    public int PageSize { get; set; } = 20;
     public string SortBy { get; set; } = string.Empty;
     public string SortDirection { get; set; } = string.Empty;
 }
