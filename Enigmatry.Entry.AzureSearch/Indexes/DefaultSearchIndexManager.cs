2c5b1aab63b137f9c8f6f8662a4806aef4a25d1b
vsljivic <v.sljivic@enigmatry.com>
2026-08-19 16:14:52 +0200

    BP-1640 Update NuGet packages

--- Enigmatry.Entry.AzureSearch/Indexes/DefaultSearchIndexManager.cs
+++ Enigmatry.Entry.AzureSearch/Indexes/DefaultSearchIndexManager.cs
@@ -1,6 +1,9 @@
-﻿using Azure.Search.Documents.Indexes;
+﻿using Azure;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
 using Enigmatry.Entry.AzureSearch.Abstractions;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace Enigmatry.Entry.AzureSearch.Indexes;
 
@@ -9,7 +12,6 @@ public class DefaultSearchIndexManager<T> : ISearchIndexManager<T>
     private readonly ISearchIndexBuilder<T> _indexBuilder;
     private readonly SearchIndexClient _client;
     private readonly ILogger<DefaultSearchIndexManager<T>> _logger;
-    private const int NoContentHttpStatusCode = 204;
 
     public DefaultSearchIndexManager(ISearchIndexBuilder<T> indexBuilder, SearchIndexClient client,
         ILogger<DefaultSearchIndexManager<T>> logger)
@@ -25,11 +27,7 @@ public class DefaultSearchIndexManager<T> : ISearchIndexManager<T>
 
         _logger.LogInformation("Deleting index: {IndexName}", index.Name);
 
-        var response = await _client.DeleteIndexAsync(index, false, cancellationToken);
-        _logger.LogDebug("Response status: {Status}", response.Status);
-
-        var result = response.Status == NoContentHttpStatusCode;
-        return result;
+        return await TryDeleteIndex(index, cancellationToken);
     }
 
     public async Task<bool> RecreateIndex(CancellationToken cancellationToken)
@@ -38,12 +36,27 @@ public class DefaultSearchIndexManager<T> : ISearchIndexManager<T>
 
         _logger.LogInformation("Delete previous index. Index name: {IndexName}", index.Name);
 
-        var deleteResponse = await _client.DeleteIndexAsync(index, false, cancellationToken);
-        _logger.LogDebug("Delete index response status: {Status}", deleteResponse.Status);
+        await TryDeleteIndex(index, cancellationToken);
 
         var response = await _client.CreateIndexAsync(index, cancellationToken);
         _logger.LogDebug("Create index response status: {Status}", response.GetRawResponse().Status);
 
         return true;
+    }
+
+    private async Task<bool> TryDeleteIndex(SearchIndex index, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var response = await _client.DeleteIndexAsync(index, false, cancellationToken);
+            _logger.LogDebug("Delete index response status: {Status}", response.Status);
+
+            return response.Status == (int)HttpStatusCode.NoContent;
+        }
+        catch (RequestFailedException e) when (e.Status == (int)HttpStatusCode.NotFound)
+        {
+            _logger.LogDebug("Index does not exist: {IndexName}", index.Name);
+            return false;
+        }
     }
 }
